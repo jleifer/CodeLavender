9e89ca58ef9456287e89999e91119bc44d487e27
Jonathan Leifer <jonathan.leifer@stonybrook.edu>
2017-04-06 15:38:05 -0400

    Initial Commit for CSE 308 Project.

---
+++ web/WEB-INF/pages/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Spartanrme
+  Date: 2/7/2017
+  Time: 8:07 PM
+  To change this template use File | Settings | File Templates.
+  from https://www.tutorialspoint.com/spring/spring_mvc_hello_world_example.htm
+--%>
+<%@ page contentType="text/html; charset=UTF-8" %>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+<h2>${message}</h2>
+</body>
+</html>
---
+++ web/resources/js/googleSignIn.js
@@ -0,0 +1,60 @@
+/**
+ * Created by Randhawa on 2/7/17.
+ */
+var userId;
+var firstName;
+var fullName;
+var imgSrc;
+var email;
+var plusUrl = "https://plus.google.com/";
+var profile;
+var auth2;
+function onSignIn(googleUser) {
+    profile = googleUser.getBasicProfile();
+    //window.open("/lists_all","_self",false);
+    userId =  profile.getId(); // Do not send to your backend! Use an ID token instead.
+    localStorage.setItem("userUrl",plusUrl.concat(userId));
+    firstName =  profile.getGivenName();
+    localStorage.setItem("firstName",firstName);
+    fullName = profile.getName();
+    localStorage.setItem("fullName",fullName);
+    imgSrc = profile.getImageUrl();
+    localStorage.setItem("imgSrc",imgSrc);
+    email =  profile.getEmail(); // This is null if the 'email' scope is not present.
+    localStorage.setItem("email",email);
+    $("#loginemail").val(email);
+    $("#usernamehiddenform").submit();
+
+}
+function onLoad() {
+    gapi.load('auth2', function() {
+        auth2 = gapi.auth2.init({
+        });
+    });
+    $("#FirstName").text(localStorage.getItem("firstName"));
+    $("#FullName").text(localStorage.getItem("fullName"));
+    $("#Email").text(localStorage.getItem("email"));
+    $("img").attr("src",localStorage.getItem("imgSrc"));
+    $("#ProfileLink").attr("href",localStorage.getItem("userUrl"));
+
+
+}
+
+function loadAuth(){
+
+    gapi.load('auth2', function() {
+        gapi.auth2.init();
+
+    });
+    auth2 = gapi.auth2.getAuthInstance();
+}
+
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+    window.open("/index","_self",false);
+}
+
