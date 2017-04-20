8f56c617ab3142e99f2ce0bb6bfb734c74600711
AjaxSurangama <cyfbjp000@hotmail.com>
2017-04-20 14:41:05 -0400

    DataStore, automatically init New Course / Module / topic/quiz 2

--- src/cLPackage/NewCourseController.java
+++ src/cLPackage/NewCourseController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
 @RequestMapping({"/newCourse.jsp","/newCourse"})
 public class NewCourseController {
@@ -27,17 +29,14 @@ public class NewCourseController {
         User curUser = ObjectifyService.ofy().load().type(User.class).id(Long.parseLong(userId)).now();
 
         //create a course / module / topic for curUser, and save into datastore.
-        Course newCourse = new Course("Course Name", curUser.getFirstName(), curUser.getLastName(),
+        Course newCourse = new Course("No Course Name", curUser.getFirstName(), curUser.getLastName(),
                 0, 0,0,"This user is to lazy to put a description", curUser);
         System.out.print(newCourse.getName());
         ObjectifyService.ofy().save().entity(newCourse).now();
-
         Module newModule = new Module("Default Module",newCourse);
         ObjectifyService.ofy().save().entity(newModule).now();
-
         Topic newTopic = new Topic("Default Topic",0,"No Content",newModule);
         ObjectifyService.ofy().save().entity(newTopic).now();
-
         String options[] = {"True","False"};
         MultipleChoices newMulti= new MultipleChoices("Default Question Text",2,1,options);
         ObjectifyService.ofy().save().entity(newMulti).now();
@@ -47,10 +46,10 @@ public class NewCourseController {
         Long newCourseId = ObjectifyService.ofy().load().entity(newCourse).now().id;
 
         //set data for future use
-        model.addAttribute("userId",userId);
+        List<Module> modules = ObjectifyService.ofy().load().type(Module.class).ancestor(curUser).list();
+        model.addAttribute("userId",Long.parseLong(userId));
         model.addAttribute("courseId",newCourseId);
-        model.addAttribute("moduleNum",1); // 1 default module
-
+        model.addAttribute("courseObj",newCourse);
         return "newCourse"; //Name of the jsp - using a different name will result in a different jsp being loaded.
     }
 }
